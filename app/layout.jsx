b8fcc9854a6c6c5256f9c36675c747acf22fbc2a
Beatrix T. <33416452+beatrixtrk@users.noreply.github.com>
2024-02-23 12:53:00 +0100

    favicon, og

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,12 +13,20 @@ const bellefair = Bellefair({ subsets: ['latin'], weight: ['400'] });
 export const metadata = {
 	title: 'Space Tourism Website',
 	description: 'Frontend Mentor Project',
+	openGraph: {
+		images: 'shared/space-tourism-og.png',
+	},
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head>
+				<link
+					rel="shortcut icon"
+					href="shared/favicon.ico"
+					type="image/x-icon"
+				/>
 				<link
 					rel="stylesheet"
 					href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;700&family=Barlow+Condensed:wght@400;700&family=Bellefair:wght@400&display=swap"
